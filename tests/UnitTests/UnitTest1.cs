553927d2c5006051fe078eb2dd6bb78bd24ab69b
Sairam Badisa <sairambadisa9@gmail.com>
2019-02-08 15:33:33 -0600

    commit

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,32 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Mul_WorksWithTwoPositiveIntegers()
         {
+            var expected = 15;
+            var a = 5;
+            var b = 3;
+            var actual = Mul(5, 3);
+            Assert.Equal(expected, actual);
+        }
 
+        int Mul(int x, int y)
+        {
+            return x * y;
+        }
+
+         bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(10)]
+        public void IsEven_WorksWithManyInputs(int value)
+        {
+            Assert.True(IsEven(value));
         }
     }
 }
